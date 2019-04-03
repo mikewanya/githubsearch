78f8125241885a2f85ae22c6ed2d3286dce68ef1
mikewanya <wanyamalately@gmail.com>
2019-04-03 16:08:59 +0300

    profile pulled with bio data

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,13 +10,14 @@ import { SearchService } from '../services/search.service';
 })
 export class SearchComponent implements OnInit {
 //phase 2 profile
-private profile:any[];
+//private  its not necessary to write private before profile below-
+ profile:any[];
 
 //add code inside constructor
   constructor(private searchService: SearchService) {
     this.searchService.getProfileInfo().subscribe(profile => {
        console.log(profile);
-       
+
        //phase 2 profile propety to be used search.component
        this.profile = profile;
      });
