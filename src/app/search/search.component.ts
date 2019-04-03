4cc97abfd429ccd945eefb94f3d76cae8f3308d4
mikewanya <wanyamalately@gmail.com>
2019-04-03 12:22:10 +0300

    pulling name and company from github

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,11 +9,16 @@ import { SearchService } from '../services/search.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+//phase 2 profile
+private profile:any[];
 
 //add code inside constructor
   constructor(private searchService: SearchService) {
     this.searchService.getProfileInfo().subscribe(profile => {
        console.log(profile);
+       
+       //phase 2 profile propety to be used search.component
+       this.profile = profile;
      });
 
    }
